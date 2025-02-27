86e5b92c0f9f745a37ed10a410f61b91a38fd217
ShaRkaSiK <ostapolga1959@gmail.com>
2025-02-27 06:52:17 +0000

    fead: add brain-event game

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,9 @@ export default [
     },
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
     },
   },
 ] // satisfies Linter.Config[]
