8fb4ef6430b0a24a82831bfea712d408026f8434
ShaRkaSiK <ostapolga1959@gmail.com>
2025-03-09 16:17:01 +0000

    feat: add brain-prime game and refactor code

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';
+const ROUNDS_COUNT = 3;
 
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = (num) => num % 2 === 0;
 
 const generateRound = () => {
   const randomNumber = Math.floor(Math.random() * 10);
   const question = `${randomNumber}`;
-  const correctAnswer = isEven(randomNumber);
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
@@ -18,17 +19,15 @@ const runGame = (gameDescription, gameRound) => {
   console.log('');
 
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('');
+  console.log(`Hello, ${name}!\n`);
 
-  const roundsCount = 3;
-  for (let i = 0; i < roundsCount; i += 1) {
+  for (let i = 0; i < ROUNDS_COUNT; i += 1) {
     const [question, correctAnswer] = gameRound();
 
     console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
 
-    if (userAnswer.toString() === correctAnswer.toString()) {
+    if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
@@ -40,6 +39,6 @@ const runGame = (gameDescription, gameRound) => {
   console.log(`Congratulations, ${name}!`);
 };
 
-const startGame = () => runGame(description, generateRound);
+const startGame = () => runGame(DESCRIPTION, generateRound);
 
 startGame();
